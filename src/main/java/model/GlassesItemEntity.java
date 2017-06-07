20f75a637500cbf1b31d7c9ba2900e8517038c65
86761 <867616074@qq.com>
2017-06-07 16:14:56 +0800

    1

--- src/main/java/model/GlassesItemEntity.java
+++ src/main/java/model/GlassesItemEntity.java
@@ -3,7 +3,7 @@ package model;
 import java.math.BigDecimal;
 
 /**
- * Created by 86761 on 2017/6/5.
+ * Created by 86761 on 2017/6/7.
  */
 public class GlassesItemEntity {
     private int glassesItemId;
@@ -11,6 +11,8 @@ public class GlassesItemEntity {
     private String lensType;
     private String lensColor;
     private String lensMaterial;
+    private int frameId;
+    private int presId;
 
     public int getGlassesItemId() {
         return glassesItemId;
@@ -52,6 +54,22 @@ public class GlassesItemEntity {
         this.lensMaterial = lensMaterial;
     }
 
+    public int getFrameId() {
+        return frameId;
+    }
+
+    public void setFrameId(int frameId) {
+        this.frameId = frameId;
+    }
+
+    public int getPresId() {
+        return presId;
+    }
+
+    public void setPresId(int presId) {
+        this.presId = presId;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -60,6 +78,8 @@ public class GlassesItemEntity {
         GlassesItemEntity that = (GlassesItemEntity) o;
 
         if (glassesItemId != that.glassesItemId) return false;
+        if (frameId != that.frameId) return false;
+        if (presId != that.presId) return false;
         if (glassesPrice != null ? !glassesPrice.equals(that.glassesPrice) : that.glassesPrice != null) return false;
         if (lensType != null ? !lensType.equals(that.lensType) : that.lensType != null) return false;
         if (lensColor != null ? !lensColor.equals(that.lensColor) : that.lensColor != null) return false;
@@ -75,6 +95,8 @@ public class GlassesItemEntity {
         result = 31 * result + (lensType != null ? lensType.hashCode() : 0);
         result = 31 * result + (lensColor != null ? lensColor.hashCode() : 0);
         result = 31 * result + (lensMaterial != null ? lensMaterial.hashCode() : 0);
+        result = 31 * result + frameId;
+        result = 31 * result + presId;
         return result;
     }
 }
